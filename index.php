ff9f1a27e6bb8e0b08509b6e5020f6979d37a1bf
Aequin0x <thibaut.desrame@gmail.com>
2016-07-04 16:44:11 +0200

    add remember + co/deco + crypt mdp

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 require 'config.php';
 var_dump($_SESSION);
 $_SESSION['IP'] = $_SERVER['REMOTE_ADDR'];
+
+setcookie('cookie', 'test', time()+60*60*24*365, '/cours/36auth', 'localhost', FALSE, TRUE);
+var_dump($_COOKIE);
 ?>
 <!DOCTYPE html>
 <html>
@@ -51,6 +54,7 @@ if(!isset($_SESSION['id'])){
 		<br>
 		<input type="text" name="password">
 	</div>
+	<input type="checkbox" name="remember">Se souvenir de moi
 	<div>
 		<button name="loginForm">Se connecter</button>
 	</div>
@@ -70,15 +74,28 @@ var_dump(password_verify("test", $crypt));
 */
 	if(isset($_POST['send'])){
 		$date = time();
+		$login = $_POST['login'];
+		$email = $_POST['email'];
 		$password = password_hash(trim($_POST['password']), PASSWORD_DEFAULT);
 
-		$query= $db->prepare("INSERT INTO user(login, email, password, date) VALUES(:login, :email, :password, :date)");
-		$query->bindValue(":login", $_POST['login'], PDO::PARAM_STR);
-		$query->bindValue(":email", $_POST['email'], PDO::PARAM_STR);
-		$query->bindValue(":password", $password, PDO::PARAM_STR);
-		$query->bindValue(":date", $date, PDO::PARAM_INT);
-		$query->execute();
-		echo "Ok";
+		$checkUser = $db->prepare("SELECT * FROM user WHERE login = :login");
+		$checkUser->bindValue(":login", $login, PDO::PARAM_STR);
+		$checkUser->execute();
+		if(!$checkUser->rowCount()){
+			$query= $db->prepare("INSERT INTO user(login, email, password, date) VALUES(:login, :email, :password, :date)");
+			$query->bindValue(":login", $login, PDO::PARAM_STR);
+			$query->bindValue(":email", $email, PDO::PARAM_STR);
+			$query->bindValue(":password", $password, PDO::PARAM_STR);
+			$query->bindValue(":date", $date, PDO::PARAM_INT);
+			// On execute la requête et si elle fonction elle renvoie true
+			if($query->execute()){
+				$_SESSION['id'] = $db->lastInsertId();
+				$_SESSION['login'] = $login;
+				header("Location: ".$url);
+			}
+		}else {
+			echo "L'utilisateur existe déjà.";
+		}
 	}
 ?>
 
@@ -97,6 +114,11 @@ if(isset($_POST['loginForm'])){
 			$user = $query->fetch();
 			$valid= password_verify($password, $user['password']);
 			if($valid){
+				if(isset($_POST['remember'])){
+					$token = sha1(md5(uniqid().$_SERVER['REMOTE_ADDR']));
+					setcookie('remember',$token, time()+60*60+24);
+					$db->query('UPDATE FROM user SET token = $token WHERE id ='$user['id']);
+				}
 				$_SESSION['id'] = $user['id'];
 				$_SESSION['login'] = $user['login'];
 				header("Location: ".$url);
