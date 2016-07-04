d248e245f086bf89b11eb572c4c41017f8a29e40
Aequin0x <thibaut.desrame@gmail.com>
2016-07-04 10:07:52 +0200

    first

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * Connexion à la base de données
+*/
+define('HOST', 'localhost');
+define('USER', 'root');
+define('PASS', '');
+define('DB', 'cookie');
+
+$options_db = array(
+	PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+	PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,
+	PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+);
+
+$db = new PDO("mysql:host=".HOST.";dbname=".DB, USER, PASS, $options_db);
+?>
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Connexion session</title>
+</head>
+<body>
+	<fieldset>
+		
+	</fieldset>
+</body>
+</html>
